19f528fabd309dce7e0ce13ca931832af5676675
Godana <godanaemiru@gmail.com>
2021-07-11 03:19:38 +0300

    added username field

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://blob.sololearn.com/avatars/52f39b23-ad4c-42c6-829a-6fd70605a319.jpg" class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="pl-5"><h1>Godana Emiru</h1></div>
+            <div class="d-flex pl-5 pt-2">
+                <div class="pr-5 pt-1"><strong>38 </strong> followers </div>
+                <div class="pr-5 pt-1"><strong>38 </strong> followers </div>
             </div>
+
+            <div class="pl-5 "><strong>13 </strong> courses </div> 
+
+            <div class="pl-5 pt-3"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quas delectus. </div> 
+
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4 ">
+            <img src="https://images.unsplash.com/photo-1516357231954-91487b459602?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80" alt="" class="w-100 rounded-lg">
+        </div>
+        <div class="col-4">
+            <img src="https://images.unsplash.com/photo-1516357231954-91487b459602?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80" alt="" class="w-100 rounded-lg">
+        </div>
+        <div class="col-4">
+            <img src="https://images.unsplash.com/photo-1516357231954-91487b459602?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80" alt="" class="w-100 rounded-lg">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>AdvancedEdu</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    AdvancedEdu
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -51,7 +51,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->username }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
