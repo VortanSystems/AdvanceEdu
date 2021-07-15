8ccec0514a4bfbc38a13a623d97bb28e536748c9
Godana <godanaemiru@gmail.com>
2021-07-15 03:05:49 +0300

    Course create configured

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -2,17 +2,19 @@
 
 @section('content')
 <div class="container">
-    Courses
 
     <div class="row pt-5">
-
+        <div class="pl-4 d-flex justify-content-between">
+        <h2> Courses</h2>
+        <a href="">Create New Course</a>
+        </div>
     </div>
     <div class="row pt-5">
 
         @foreach ($courses as $course)
         <div class="col-4 ">
             <div class="card">
-                <img class="card-img-top" src="/storage/app/public/profile/2br1SxCI16po6Z5RWr8ux5QPefhof2IPblXqKupm.jpeg" alt="">
+                <img class="card-img-top" src="/storage/profile/temp_course_holder.jpg" alt="">
                 <div class="card-body">
                     <h4 class="card-title" >{{ $course->title }}</h4>
                     <p class="card-text" style="font-size: 16px !important;line-height: 24px !important;font-weight: 400 !important;color: #717171 !important;">{{ $course->description }}</p>
